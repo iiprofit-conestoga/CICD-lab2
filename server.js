70e3a79915c829ef878746a8858d3d7e5aea808c
iiprofit-conestoga <ksolanki6269@conestogac.on.ca>
2025-03-15 22:59:03 -0400

    test comment added

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 const app = express();
 const port = 3000;
 
+// Test comment added
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
